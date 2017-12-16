051c4b74747d2299c831746ca964f8416a65fe89
chrisjxn <usjacksons@gmail.com>
2017-12-15 17:36:54 -0700

    removed components related to devmountain-simulation-mastery-01

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-import CSSBasics from './components/cssBasics/CSSBasics';
-import HTML5Basics from './components/html5Basics/HTML5Basics';
 
 class App extends Component {
   render() {
@@ -16,8 +14,6 @@ class App extends Component {
           <div>
             <Switch>
               <Route path='/' component={Home} exact />
-              <Route path='/cssbasics' component={CSSBasics} />
-              <Route path='/html5basics' component={HTML5Basics} />
             </Switch>
           </div>
         </div>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Home.css';
 
 export default function Home() {
     return (
         <div className="homeComponent">
             <div>This is the Home component</div>
-            <div><Link to='/cssbasics'>CSS Basics</Link></div>
-            <div><Link to='/html5basics'>HTML5 Basics</Link></div>
         </div>
     )
 }
